fab1403875d7b3d9a23cde3c0159f672b4f97fa1
Thiago Fontanella <thiago.fontanella@hotmail.com>
2024-07-05 23:41:20 -0300

    feature: login form structure

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,56 @@
 // import './Login.scss'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GoogleIcon from '@mui/icons-material/Google';
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className="login">
+            <div className="login__container">
+                <form className="login__form">
+                    <p className='login__form--title'>Entrar</p>
+                    <fieldset>
+                        <legend>
+                            Nome de usuário
+                        </legend>
+                        <label htmlFor="login_username">
+                            <input 
+                                type="text" 
+                                name="login_username" 
+                                id="login_username"
+                                placeholder='Nome de usuário'
+                            />
+                        </label>
+                    </fieldset>
+                    <fieldset>
+                        <legend>
+                            Senha
+                        </legend>
+                        <label htmlFor="login_password">
+                            <input 
+                                type="password" 
+                                name="login_password" 
+                                id="login_password" 
+                                placeholder='Senha'
+                            />
+                        </label>
+                    </fieldset>
+                    <button 
+                        type="submit"
+                        className='login__submit'
+                    >
+                        Entrar
+                    </button>
+                </form>
+                <hr />
+                <div className='login__social-media'>
+                    <button className='login__social-media--google'>
+                        <GoogleIcon /> Entrar com Google
+                    </button>
+                    <button className='login__social-media--facebook'>
+                        <FacebookIcon /> Entrar com Facebook
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
