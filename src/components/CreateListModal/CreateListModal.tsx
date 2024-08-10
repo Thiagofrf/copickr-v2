b3075fe9528fac7961ac607862d8e993916a5f9b
Thiago Fontanella <thiago.fontanella@hotmail.com>
2024-08-10 01:10:05 -0300

    feature: create list modal

--- src/components/CreateListModal/CreateListModal.tsx
+++ src/components/CreateListModal/CreateListModal.tsx
@@ -4,7 +4,34 @@ const CreateListModal = () => {
     return (
         <>
             <div className="create-modal">
-                Create List
+                <div className="create-modal__content">
+                    <h2>
+                        Crie sua lista
+                    </h2>
+                    <p>
+                        Registre os dados iniciais, depois você pode complementar sua lista com qualquer informação.
+                    </p>
+                    <form>
+                        <fieldset>
+                            <label htmlFor="">
+                                Nome da lista
+                            </label>
+                        </fieldset>
+                        <fieldset>
+                            <label htmlFor="">
+                                Categoria
+                            </label>
+                        </fieldset>
+                        <fieldset>
+                            <label htmlFor="">
+                                Primeiro item da lista
+                            </label>
+                        </fieldset>
+                        <button>
+                            Iniciar minha lista
+                        </button>
+                    </form>
+                </div>
             </div>
         </>
     )
